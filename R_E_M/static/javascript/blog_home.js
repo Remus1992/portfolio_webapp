4c212dc2dc9ae80c3b32445bd9b9e76a608e5a08
Remus1992 <Remington.Henderson@gmail.com>
2018-09-12 08:19:58 -0700

    Sending Blog Button Updates

--- R_E_M/static/javascript/blog_home.js
+++ R_E_M/static/javascript/blog_home.js
@@ -11,38 +11,7 @@ $('.checkbox').click(function () {
     });
 
     $(this).addClass('checkFocus');
-});
 
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        let cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            let cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-let csrftoken = getCookie('csrftoken');
-
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
-$.ajaxSetup({
-    beforeSend: function (xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
 });
 
 $('.checkbox').click(function () {
@@ -70,3 +39,24 @@ span.onclick = function () {
     $("div.blog_gallery").html(blog_html);
     $('#blog_content').trumbowyg();
 };
+
+// console.log(document.location.toString().split("?category=")[1]);
+
+let blog_url_slug = document.location.toString().split("?category=")[1];
+
+if (blog_url_slug === "undefined") {
+    console.log("Element is Undefined")
+} else {
+    console.log("Element is " + blog_url_slug)
+}
+
+$('.checkbox').each(function (i, el) {
+    console.log(el.id);
+    $(el).removeClass('checkFocus');
+    if (el.id === document.location.toString().split("?category=")[1]){
+        console.log("Success!");
+        $(this).addClass('checkFocus');
+    }
+});
+
+
