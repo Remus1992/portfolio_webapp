e97548ef5cc81a3bdf8e0f1566c648e3a2d082aa
Remus1992 <Remington.Henderson@gmail.com>
2018-10-02 14:56:21 -0700

    Sending JS updates

--- R_E_M/static/javascript/home.js
+++ R_E_M/static/javascript/home.js
@@ -25,22 +25,23 @@ $(document).ready(function () {
         gather_offset('#M3', {'opacity': '0.8', 'top': '-250px', 'left': '1350px'});
         gather_offset('#M4', {'opacity': '0.8', 'top': '250px'});
 
-        setTimeout(function () {
-            $('#loader').hide('slow');
-            ani("#R1", offsets["#R1"], 2000);
-            ani("#R2", offsets["#R2"], 2000);
-            ani("#R3", offsets["#R3"], 2000);
-            ani("#R4", offsets["#R4"], 2000);
-            ani("#E1", offsets["#E1"], 2000);
-            ani("#E2", offsets["#E2"], 2000);
-            ani("#E3", offsets["#E3"], 2000);
-            ani("#E4", offsets["#E4"], 2000);
-            ani("#M1", offsets["#M1"], 2000);
-            ani("#M2", offsets["#M2"], 2000);
-            ani("#M3", offsets["#M3"], 2000);
-            ani("#M4", offsets["#M4"], 2000);
-        }, 500);
-
+        $( document ).ready(function() {
+            setTimeout(function () {
+                $('#loader').hide('slow');
+                ani("#R1", offsets["#R1"], 2000);
+                ani("#R2", offsets["#R2"], 2000);
+                ani("#R3", offsets["#R3"], 2000);
+                ani("#R4", offsets["#R4"], 2000);
+                ani("#E1", offsets["#E1"], 2000);
+                ani("#E2", offsets["#E2"], 2000);
+                ani("#E3", offsets["#E3"], 2000);
+                ani("#E4", offsets["#E4"], 2000);
+                ani("#M1", offsets["#M1"], 2000);
+                ani("#M2", offsets["#M2"], 2000);
+                ani("#M3", offsets["#M3"], 2000);
+                ani("#M4", offsets["#M4"], 2000);
+            }, 1000);
+        });
 
         function gather_offset(id, css) {
             offsets[id] = $(id).offset();
